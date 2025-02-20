738156e5f3ac5a16c29757ee689f2468849b1653
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-02-21 01:10:07 +0530

    Change Backend port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 initializeSocket(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
