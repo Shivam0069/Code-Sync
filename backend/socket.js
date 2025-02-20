8f225754e052563d1104ee17a5667db951d57bc7
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-02-20 23:52:40 +0530

    backend server and socket setup

---
+++ backend/socket.js
@@ -0,0 +1,20 @@
+const socketIO = require("socket.io");
+
+let io;
+
+const initializeSocket = (server) => {
+  io = socketIO(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`New client connected: ${socket.id}`);
+  });
+};
+
+module.exports = {
+  initializeSocket,
+};
